f6ca1f22f59e4e3b1cb82c486efecae4b65b906e
Miroslav <regdayz@gmail.com>
2020-11-10 19:10:26 +0300

    Project creation

---
+++ IndigoGame/Source/IndigoGame/Framework/IndigoGameGameInstance.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Engine/GameInstance.h"
+#include "IndigoGameGameInstance.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class INDIGOGAME_API UIndigoGameGameInstance : public UGameInstance
+{
+	GENERATED_BODY()
+	
+};
---
+++ IndigoGame/Source/IndigoGame/Framework/IndigoGameGameStateBase.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameStateBase.h"
+#include "IndigoGameGameStateBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class INDIGOGAME_API AIndigoGameGameStateBase : public AGameStateBase
+{
+	GENERATED_BODY()
+	
+};
